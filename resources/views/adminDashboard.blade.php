7a4ef150d2436cd9363e3ffd8324280af63a9025
Xander Wauters <xwauters@outlook.com>
2024-05-26 12:08:38 +0200

    Added styling to the admin dashboard

--- resources/views/adminDashboard.blade.php
+++ resources/views/adminDashboard.blade.php
@@ -11,24 +11,19 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
-    
+
     <title>Admin Dashboard</title>
 </head>
 <body>
-    <header class="header1"><img class="logo1" src="{{ asset('Assets/Logo/horizontaal EhB-logo (transparante achtergrond).png')}}" alt="logo-ehb=horizontal"></header>
     <main class="maincontainer">
+        <img src="{{ asset('assets/logo/logo-trans.png') }}" alt="logo EhB">
         <div class="container">
-            <a href="{{asset('/Klaarzetten')}}" class="Klaarzetten container1">Klaarzetten</a>
-            <a href="{{asset('/terugbrengen')}}" class="QR-scanner container1">Terugbrengen</a>
-            <a href="{{asset('/Bezetscherm')}}" class="Beschikbaarheid container1">Beschikbaarheid</a>
+            <a href="{{asset('/Klaarzetten')}}" class="Klaarzetten">Klaarzetten</a>
+            <a href="{{asset('/terugbrengen')}}" class="QR-scanner">Terugbrengen</a>
+            <a href="{{asset('/Bezetscherm')}}" class="Beschikbaarheid">Beschikbaarheid</a>
+            <a href="{{asset('/Producttoevoegen')}}" class="Productbeheer">Productbeheer</a>
+            <a href="{{asset('/Banoverzicht')}}" class="Blacklist">Blacklist</a>
         </div>
-        <div class="kontainer">
-            <a href="{{asset('/Producttoevoegen')}}" class="Productbeheer kontainer2">Productbeheer</a>
-            <a href="{{asset('/Banoverzicht')}}" class="Blacklist kontainer2">Blacklist</a>
-        </div>
-
     </main>
-    <footer class="footer"></footer>
-
 </body>
 </html>
