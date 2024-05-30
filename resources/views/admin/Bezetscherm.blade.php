c9a73287b5244c56b8e98ed55c06228733c68e55
lucasmoons <lucas.moons@student.ehb.be>
2024-05-30 15:24:40 +0200

    Fixed the bugs with the filtering in Bezetscherm. (status that changes when page is refresched and search term that disappears when status is clicked)

--- resources/views/admin/Bezetscherm.blade.php
+++ resources/views/admin/Bezetscherm.blade.php
@@ -4,8 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Bezetscherm | Admin</title>
-    <link rel="icon" href="{{asset('Assets/Logo/logo.png')}}" type="image/png">
-    <link rel="stylesheet" href="{{asset('css/Admin/Bezetscherm.css')}}">
+    <link rel="icon" href="{{ asset('Assets/Logo/logo.png') }}" type="image/png">
+    <link rel="stylesheet" href="{{ asset('css/Admin/Bezetscherm.css') }}">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900&display=swap" rel="stylesheet">
@@ -24,9 +24,11 @@
             <tr>
                 <th>Product</th>
                 <th id="status-header">
-                    <form action="{{ route('index') }}" method="get">
+                    <form action="{{ route('index') }}" method="get" id="status-form">
                         <input type="hidden" name="action" value="filter">
-                        <a href="{{ route('index') }}?action=filter">Status</a>
+                        <input type="hidden" name="status_action" value="toggle">
+                        <input type="hidden" name="query" value="{{ $searchQuery ?? '' }}">
+                        <a href="#" onclick="document.getElementById('status-form').submit();">Status</a>
                     </form>
                 </th>
                 <th>Student</th>
@@ -48,7 +50,7 @@
     </table>
 
     <div class="paginationStyle">
-        {{ $products->appends(['query' => $searchQuery, 'action' => $action])->links('pagination::bootstrap-4') }}
+        {{ $products->appends(['query' => $searchQuery, 'action' => $action, 'isFiltering' => $isFiltering])->links('pagination::bootstrap-4') }}
     </div>
 
     @include('jsAdmin.Bezetscherm')
