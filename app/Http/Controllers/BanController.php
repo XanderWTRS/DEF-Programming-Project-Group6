6f9fed6deadd64bff18d2d8d74fa0dd8056c9147
jornDS <117446313+Rackillu@users.noreply.github.com>
2024-05-30 10:56:37 +0200

    emails when someone banned

--- app/Http/Controllers/BanController.php
+++ app/Http/Controllers/BanController.php
@@ -5,7 +5,8 @@ namespace App\Http\Controllers;
 use App\Models\Ban;
 use Illuminate\Http\Request;
 use Carbon\Carbon;
-
+use App\Mail\BanMail;
+use Illuminate\Support\Facades\Mail;
 class BanController extends Controller
 {
     public function index()
@@ -32,19 +33,21 @@ public function banUser(Request $request)
 {
     $userId = $request->input('user_id');
     $name = $request->input('name');
+    $email = $request->input('email');
 
-    $banDate = Carbon::today()->addDays(30)->toDateString(); 
+    $banDate = Carbon::today()->addDays(30)->toDateString();
 
     $ban = new Ban();
     $ban->user_id = $userId;
     $ban->name = $name;
     $ban->status = 'banned';
-    $ban->date = $banDate; 
+    $ban->date = $banDate;
     $ban->save();
+    Mail::to($email)->send(new BanMail($name, $banDate));
 
     return response()->json(['success' => true]);
 }
-    
+
 
 public function autoUnbanUsers()
 {
